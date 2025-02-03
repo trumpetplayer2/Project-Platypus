fc9bf90692d7f4666c04a48e833c1bb36b22c8f3
trumpetplayer2 <78563622+trumpetplayer2@users.noreply.github.com>
2025-02-03 17:48:20 -0500

    Started Sense Ability

--- Assets/Scripts/Player/PlayerAbilityManager.cs
+++ Assets/Scripts/Player/PlayerAbilityManager.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 namespace tp2
@@ -18,12 +19,15 @@ namespace tp2
     {
         public float Cooldown;
         public GameObject digTip;
+        public string DigInputName = "Dig";
     }
     [Serializable]
     public class SixthSenseSettings
     {
-        public float Cooldown;
-        public SenseSphere sense;
+        public float Cooldown = 1f;
+        public float length = 10f;
+        public SenseSphere sphere;
+        public string SenseInputName = "SixthSense";
     }
     public class PlayerAbilityManager : MonoBehaviour
     {
@@ -37,6 +41,7 @@ namespace tp2
         //Sixth Sense
         public SixthSenseSettings sense;
         private float senseCooldown = 0;
+        bool canSense = false;
 
         // Start is called before the first frame update
         void Start()
@@ -46,6 +51,57 @@ namespace tp2
 
         // Update is called once per frame
         void Update()
+        {
+            if (GameManager.instance.isPaused) return;
+            DecrementCooldown();
+            if (Input.GetButtonDown(sense.SenseInputName))
+            {
+                if (canSense)
+                {
+                    Sense();
+                }
+            }
+            if (Input.GetButton(dig.DigInputName))
+            {
+                Dig();
+            }
+            Gravel();
+
+        }
+
+        void DecrementCooldown()
+        {
+            senseCooldown -= Mathf.Max(0, senseCooldown - Time.deltaTime);
+            digCooldown -= Mathf.Max(0, senseCooldown - Time.deltaTime);
+            gravelCooldown -= Mathf.Max(0, senseCooldown - Time.deltaTime);
+        }
+        void Sense()
+        {
+            sense.sphere.showDetect();
+            Invoke("SenseFade", sense.length);
+        }
+
+        void SenseFade()
+        {
+            sense.sphere.hideDetect();
+        }
+
+        void Gravel()
+        {
+            CollectGravel();
+            SpitGravel();
+        }
+        void CollectGravel()
+        {
+
+        }
+
+        void SpitGravel()
+        {
+
+        }
+
+        void Dig()
         {
 
         }
