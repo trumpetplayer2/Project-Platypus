5f1b02098121af5050ad5d2863f551ee5c589c5d
GarrettMcNaLang <112025126+GarrettMcNaLang@users.noreply.github.com>
2025-04-23 19:51:41 -0400

    Minor Bug Fixes and Working on Quests

--- Assets/Scripts/AIScripts/States/SearchState.cs
+++ Assets/Scripts/AIScripts/States/SearchState.cs
@@ -81,14 +81,12 @@ public class SearchState : BaseStateClass
     /// </summary>
     public void SearchInPlaceFunction()
     {
-        if (aiScript.aIAnimator.GetCurrentAnimatorStateInfo(0).IsName("Base.Walk"))
-        {
+        
             aiScript.aIAnimator.SetBool("Walk", false);
-        }
-        else if (aiScript.aIAnimator.GetCurrentAnimatorStateInfo(0).IsName("Base.Run"))
-        {
+        
+        
             aiScript.aIAnimator.SetBool("Run", false);
-        }
+        
 
         aiScript.aIAnimator.SetBool("Interact", true);
 
